bba4bc7d3d6506cbf9e61e26f4bdec28a069db9a
druid <amrdruid@gmail.com>
2016-11-06 21:53:27 +0200

    Adds time zone script

---
+++ scripts/time_with_zone.rb
@@ -0,0 +1,8 @@
+# Time.now give you the time of the server, neglecting your configuration of the time zone
+# Time.zone.now gives you the accurate time according the configuration in the time zone you made in application.rb
+# Example: config.time_zone = 'Singapore' 
+
+Time.now
+
+Time.zone.now
+
